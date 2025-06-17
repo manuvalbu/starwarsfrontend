069bfcd5ac8fed819f3b25e877b4394170a82edb
manu <manuvalbu@hotmail.com>
2025-06-17 23:20:17 +0200

    feat(components): sorter and filter

--- src/app/components/people-table.component/people-table.component.ts
+++ src/app/components/people-table.component/people-table.component.ts
@@ -1,26 +1,30 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SwapiService } from '../../services/swapi.service';
 import { Person } from '../../models/person.model';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   standalone: true,
   selector: 'app-people-table',
   imports: [
-    CommonModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatProgressSpinnerModule
-  ],
+      CommonModule,
+      MatTableModule,
+      MatPaginatorModule,
+      MatSortModule,
+      MatProgressSpinnerModule,
+      MatFormFieldModule,
+      MatInputModule
+    ],
   templateUrl: './people-table.component.html',
   styleUrls: ['./people-table.component.css'],
 })
-export class PeopleTableComponent implements OnInit {
+export class PeopleTableComponent implements AfterViewInit {
   isLoading = false;
   peopleDataSource = new MatTableDataSource<Person>();
   displayedColumns: string[] = [
@@ -35,22 +39,44 @@ export class PeopleTableComponent implements OnInit {
     'created'
   ];
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-
   constructor(private swapiService: SwapiService) {}
 
-  ngOnInit(): void {
-    this.loadPeople();
-  }
+  ngAfterViewInit() {
+      this.loadPeople();
+    }
 
   loadPeople(): void {
     this.isLoading = true;
     this.swapiService.getAllPeople().subscribe((res) => {
       this.peopleDataSource.data = res;
-      this.peopleDataSource.paginator = this.paginator;
-      this.peopleDataSource.sort = this.sort;
       this.isLoading = false;
     });
   }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.peopleDataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  sortData(sort: Sort) {
+      const data = this.peopleDataSource.data.slice();
+      if (!sort.active || sort.direction === '') {
+        return;
+      }
+      this.peopleDataSource.data = data.sort((a, b) => {
+        const isAsc = sort.direction === 'asc';
+        switch (sort.active) {
+          case 'name':
+            return compare(a.name ?? '', b.name ?? '', isAsc);
+          case 'created':
+            return compare(a.created ?? '', b.created ?? '', isAsc);
+          default:
+            return 0;
+        }
+      });
+    }
 }
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+  }
--- src/app/components/planets-table.component/planets-table.component.ts
+++ src/app/components/planets-table.component/planets-table.component.ts
@@ -1,26 +1,30 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SwapiService } from '../../services/swapi.service';
 import { Planet } from '../../models/planet.model';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   standalone: true,
   selector: 'app-planets-table',
   imports: [
-    CommonModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatProgressSpinnerModule
-  ],
+      CommonModule,
+      MatTableModule,
+      MatPaginatorModule,
+      MatSortModule,
+      MatProgressSpinnerModule,
+      MatFormFieldModule,
+      MatInputModule
+    ],
   templateUrl: './planets-table.component.html',
   styleUrls: ['./planets-table.component.css'],
 })
-export class PlanetsTableComponent implements OnInit {
+export class PlanetsTableComponent implements AfterViewInit {
   isLoading = false;
   planetsDataSource = new MatTableDataSource<Planet>();
   displayedColumns: string[] = [
@@ -36,22 +40,44 @@ export class PlanetsTableComponent implements OnInit {
     'created'
   ];
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-
   constructor(private swapiService: SwapiService) {}
 
-  ngOnInit(): void {
-    this.loadPlanets();
+    ngAfterViewInit() {
+        this.loadPlanets();
+      }
+
+    loadPlanets(): void {
+      this.isLoading = true;
+      this.swapiService.getAllPlanets().subscribe((res) => {
+        this.planetsDataSource.data = res;
+        this.isLoading = false;
+      });
+    }
+
+    applyFilter(event: Event): void {
+      const filterValue = (event.target as HTMLInputElement).value;
+      this.planetsDataSource.filter = filterValue.trim().toLowerCase();
+    }
+
+    sortData(sort: Sort) {
+        const data = this.planetsDataSource.data.slice();
+        if (!sort.active || sort.direction === '') {
+          return;
+        }
+        this.planetsDataSource.data = data.sort((a, b) => {
+          const isAsc = sort.direction === 'asc';
+          switch (sort.active) {
+            case 'name':
+              return compare(a.name ?? '', b.name ?? '', isAsc);
+            case 'created':
+              return compare(a.created ?? '', b.created ?? '', isAsc);
+            default:
+              return 0;
+          }
+        });
+      }
   }
 
-  loadPlanets(): void {
-    this.isLoading = true;
-    this.swapiService.getAllPlanets().subscribe((res) => {
-      this.planetsDataSource.data = res;
-      this.planetsDataSource.paginator = this.paginator;
-      this.planetsDataSource.sort = this.sort;
-      this.isLoading = false;
-    });
-  }
-}
+  function compare(a: number | string, b: number | string, isAsc: boolean) {
+      return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+    }
